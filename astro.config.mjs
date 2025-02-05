0c844a0c422f9e91fdd829fc734bd72f134ea64b
yz <x@yzlaboratory.dev>
2025-02-05 16:33:53 +0100

    social first prototype

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   vite: { plugins: [tailwindcss()] },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
