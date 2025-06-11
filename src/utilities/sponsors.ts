4d6b5c355266db3da327a7ddbbd44e7d0aa866d9
yz <x@yzlaboratory.dev>
2025-06-11 18:03:15 +0200

    impressum daten sponsoring

---
+++ src/utilities/sponsors.ts
@@ -0,0 +1,220 @@
+import allianz from "../assets/allianz.svg";
+import annetreib from "../assets/annetreib.png";
+import bbl from "../assets/bbl.png";
+import bikesport from "../assets/bikesport.png";
+import falk from "../assets/falk.png";
+import glaab from "../assets/glaab.png";
+import holzhauser from "../assets/holzhauser.svg";
+import kempf from "../assets/kempf.jpg";
+import kessler from "../assets/kessler.png";
+import kirchen from "../assets/kirchen.png";
+import lsheizung from "../assets/lsheizung.jpg";
+import malermeisterschmitd from "../assets/malermeisterschmitd.png";
+import mbfischhaus from "../assets/mbfischhaus.png";
+import metallbauwalter from "../assets/metallbauwalter.jpg";
+import opticland from "../assets/opticland.png";
+import rack from "../assets/rack.jpg";
+import reiswilhelm from "../assets/reiswilhelm.png";
+import reitenbach from "../assets/reitenbach.jpg";
+import rosport from "../assets/rosport.jpg";
+import saarmoselbaumaschinen from "../assets/saarmoselbaumaschinen.svg";
+import schmittfood from "../assets/schmittfood.jpg";
+import grohsthewes from "../assets/grohsthewes.png";
+import waigel from "../assets/waigel.png";
+import zs from "../assets/zs.webp";
+import logo from "../assets/logo.svg";
+
+export const sponsorJSON = {
+  sponsors: [
+    {
+      Name: "Udo Andres",
+      Title: "ALLIANZ Generalvertretung",
+      ImageUrl: allianz,
+      Link: "https://vertretung.allianz.de/udo.andres/",
+    },
+    {
+      Name: "BBL Mietservice",
+      Title: "Baumaschinenvermietung",
+      ImageUrl: bbl,
+      Link: "https://www.bbl-baumaschinen.de/mietservice",
+      Color: "bg-neutral-600",
+    },
+    {
+      Name: "Bikesport Scheid",
+      Title: "Profi in Sachen Fahrradsport",
+      ImageUrl: bikesport,
+      Link: "https://www.bikesport-scheid.de/",
+    },
+    {
+      Name: "Getränke Falk",
+      Title: "seit 1927",
+      ImageUrl: falk,
+      Link: "https://www.xn--getrnke-falk-jcb.de/",
+    },
+    {
+      Name: "Sporthaus Glaab",
+      Title: "Teamsportspezialist an der Saar",
+      ImageUrl: glaab,
+      Link: "https://sporthaus-glaab.de/",
+    },
+    {
+      Name: "Holzhauser GmBH",
+      Title: "Baumaschinen Vermietung & Verkauf",
+      ImageUrl: holzhauser,
+      Link: "https://www.holzhauser.info/",
+    },
+    {
+      Name: "Kempf Aussenanlagen",
+      Title: "Garten- & Landschaftsbau",
+      ImageUrl: kempf,
+      Link: "https://kempf-gmbh.com/",
+    },
+    {
+      Name: "Rolladen Kessler GmbH",
+      Title: "innovative Lösungen für Häuser, Wohnungen und Terrassen",
+      ImageUrl: kessler,
+      Link: "https://www.kessler-saarland.com/",
+    },
+    {
+      Name: "Erik Kirchen",
+      Title: "Stuckateurmeister",
+      ImageUrl: kirchen,
+      Link: "https://kirchen-stuckateur.de/",
+    },
+    {
+      Name: "LS",
+      Title: "Heizung & Sanitär",
+      ImageUrl: lsheizung,
+      Link: "https://www.ls-heizung.de/",
+    },
+    {
+      Name: "Michael Schmidt",
+      Title: "Malermeister",
+      ImageUrl: malermeisterschmitd,
+      Link: "https://www.malerschmidt.eu/",
+      Color: "bg-zinc-100",
+    },
+    {
+      Name: "MB Fischhaus",
+      Title: "Meeresspezialitäten",
+      ImageUrl: mbfischhaus,
+      Link: "https://www.mb-fischhaus.de/",
+    },
+    {
+      Name: "Metallbau Walter",
+      Title: "Spenglerei seit 1956",
+      ImageUrl: metallbauwalter,
+      Link: "https://www.metallbau-walter.info/",
+    },
+    {
+      Name: "Opticland",
+      Title: "die Brille",
+      ImageUrl: opticland,
+      Link: "https://www.opticland-die-brille.de/",
+      Color: "bg-yellow-300",
+    },
+    {
+      Name: "Beerdigunsinstitut Rack",
+      Title: "Tradition seit 20 Jahren",
+      ImageUrl: rack,
+      Link: "https://beerdigungsinstitut-rack.de/",
+    },
+    {
+      Name: "Reis & Wilhelm",
+      Title: "Fliesenleger in der 3. Generation",
+      ImageUrl: reiswilhelm,
+      Link: "https://www.reis-wilhelm.de/",
+    },
+    {
+      Name: "Autohaus Reitenbach",
+      Title: "50 Jahre GmbH",
+      ImageUrl: reitenbach,
+      Link: "https://www.mercedes-benz-reitenbach.de/",
+    },
+    {
+      Name: "Baustoffe Rosport",
+      Title: "Bauen Sie auf uns",
+      ImageUrl: rosport,
+      Link: "https://www.baustoffe-rosport.de/",
+    },
+    {
+      Name: "Saar-Mosel Baumaschinen",
+      Title: "von Spezialisten für Spezialisten",
+      ImageUrl: saarmoselbaumaschinen,
+      Link: "https://www.saar-mosel-baumaschinen.de/",
+    },
+    {
+      Name: "Schmitt Food",
+      Title: "Qualität seit 1893",
+      ImageUrl: schmittfood,
+      Link: "https://www.schmitt-food.net/",
+    },
+    {
+      Name: "Gasthaus Grohs-Thewes",
+      Title: "Tradition seit 1905",
+      ImageUrl: grohsthewes,
+      Link: "https://www.grohs-thewes.de/",
+      Color: "bg-rose-200",
+    },
+    {
+      Name: "Gartenbau Waigel",
+      Title: "Gestaltung & Pflege",
+      ImageUrl: waigel,
+      Link: "https://www.gartenbau-waigel.de/",
+    },
+    {
+      Name: "Zimmer & Schu Gmbh",
+      Title: "Ihr kompetenter Auto-Service",
+      ImageUrl: zs,
+      Link: "https://www.zsmobile.de/",
+    },
+    {
+      Name: "Giebel Bestattungen",
+      Title: "Ihr kompetenter Auto-Service",
+      ImageUrl: logo,
+      Link: "https://www.zsmobile.de/",
+    },
+    {
+      Name: "Gasthaus zur Starz",
+      Title: "Dörsdorf",
+      ImageUrl: logo,
+      Link: "https://www.tripadvisor.de/Restaurant_Review-g1081425-d14989641-Reviews-Gasthaus_zur_Starz-Lebach_Saarland.html",
+    },
+    {
+      Name: "Rieker Physiotherapie",
+      Title: "Mühlenstr. 57, Thalexweiler",
+      ImageUrl: logo,
+      Link: "https://www.thalexweiler.de/portfolio-item/rieker-physiotherapie/",
+    },
+    {
+      Name: "Paulus und Korneker",
+      Title: "Schneidwerkzeuge",
+      ImageUrl: logo,
+      Link: "https://www.thalexweiler.de/portfolio-item/paulus-und-korneker-gmbh/",
+    },
+    {
+      Name: "Bestattungen Giebel",
+      Title: "Zur Erzkaul 1, Thalexweiler",
+      ImageUrl: logo,
+      Link: "https://www.thalexweiler.de/portfolio-item/bestattungen-giebel/",
+    },
+    {
+      Name: "AT HaArTrium",
+      Title: "Mühlenstr. 29, Thalexweiler",
+      ImageUrl: logo,
+      Link: "https://www.thalexweiler.de/portfolio-item/at-haartrium/",
+    },
+    {
+      Name: "Sandra Maione",
+      Title: "Friseursalon",
+      ImageUrl: logo,
+      Link: "https://branchenbuch.meinestadt.de/schmelz/company/500036897",
+    },
+    {
+      Name: "Hans-Jürgen Müller",
+      Title: "Sachverständiger",
+      ImageUrl: logo,
+      Link: "https://www.sachverstaendiger-mueller.de/index.html",
+    },
+  ],
+};
